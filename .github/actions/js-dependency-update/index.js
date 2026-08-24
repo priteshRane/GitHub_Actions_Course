cd00ded41ff5628c12c4c442aec7858932a9248e
Pritesh Rane <priteshrane@Mihirs-MacBook-Pro.local>
2026-08-24 22:46:27 +0530

    feat-(17 custom actions) validate inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,6 +1,50 @@
 const core = require('@actions/core');
+const exec= require('@actions/exec');
+
+const validateBranchName = ({branchName}) => /^[a-zA-Z0-9_\-\,\/]+$/.test(branchName);
+const validateDirectoryName = ({branchName}) => /^[a-zA-Z0-9_\-\/]+$/.test(branchName);
 
 async function run() {
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const ghToken = core.getInput('gh-token');
+    const workingDir = core.getInput('working-directory')
+    const debug = core.getBooleanInput('debug')
+
+    core.setSecret(ghToken);
+
+    if (validateBranchName({branchName: baseBranch})) {
+        core.setFailed('Invalid base branch name')
+        return;
+    }
+
+    if (validateBranchName({branchName: targetBranch})) {
+        core.setFailed('Invalid target branch name')
+        return;
+    }
+    
+    if (!validateDirectoryName({dirName: workingDir})) {
+        core.setFailed('Invalid working dir')
+        return;
+    }
+
+    core.info('base branch is: ${baseBranch}');
+    core.info('base branch is: ${targetBranch}');
+    core.info('base branch is: ${workingDir}');
+
+    await exec.exec('npm update', [], {
+        cwd: workingDir
+    });
+
+    const gitStatus = await exec.getExecOutput('get status -s package.json', [], {
+        cwd: workingDir
+    });
+
+    if (gitStatus.stdout.length > 0) {
+        core.info('There are updates available');
+    } else {
+        core.info('No updates point in time');
+    }
     /*
     1. Parse inputs:
         1.1 base-branch from which to check for updates
